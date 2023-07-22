92ba2c39f3b1d686706af89fab5dd67196fc76a1
Bimsara Walallawita <bwalallawita@gmail.com>
2023-07-22 12:39:09 +0530

    Order Creation and CORS

--- src/main/java/com/ijse/database/controller/OrderController.java
+++ src/main/java/com/ijse/database/controller/OrderController.java
@@ -5,13 +5,17 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ijse.database.entity.Order;
 import com.ijse.database.service.OrderService;
 
+@CrossOrigin(origins="*")
 @RestController
 @RequestMapping("/orders")
 public class OrderController {
@@ -26,6 +30,12 @@ public class OrderController {
     @GetMapping
     public ResponseEntity<List<Order>> getAllOrders() {
         return ResponseEntity.status(HttpStatus.OK).body(orderService.getAllOrders());
+    }
+
+    @PostMapping
+    public ResponseEntity<Order> createOrder(@RequestBody Order order) {
+        Order createdOrder = orderService.createOrder(order);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdOrder);
     }
 
     
